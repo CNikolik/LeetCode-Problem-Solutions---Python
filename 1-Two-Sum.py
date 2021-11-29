8bc665d79eec9178133020b21e205974550ad126
CNikolik <71856457+CNikolik@users.noreply.github.com>
2021-11-29 14:20:48 -0500

    Create 1-Two-Sum.py

---
+++ 1-Two-Sum.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/two-sum/
+# Easy
+# Submission Stats: 
+# Runtime: 3564 ms, faster than 26.75% of Python online submissions for Two Sum.
+# Memory Usage: 14.2 MB, less than 91.55% of Python online submissions for Two Sum.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        for i in range(len(nums)):
+            
+            for j in range(i + 1, len(nums)):
+                
+                if nums[i] + nums[j] == target:
+                    
+                    return [i, j]  
